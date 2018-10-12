cecf0438620a6cf6662ef94aeb095cb15a6f9cd7
antonella <antonella.filieri@gmail.com>
2018-10-12 10:32:48 +0200

    no message

--- Catalog/src/test/java/com/TestPropertiesFile.java
+++ Catalog/src/test/java/com/TestPropertiesFile.java
@@ -1,10 +1,9 @@
 package com;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ResourceBundle;
-
-import static org.hamcrest.CoreMatchers.*;
 
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
@@ -27,7 +26,6 @@ public class TestPropertiesFile {
 		assertTrue(StringUtils.isNotBlank(rb.getString("exempt.goods")));
 		assertTrue(StringUtils.isNotBlank(rb.getString("base.rate")));
 		assertTrue(StringUtils.isNotBlank(rb.getString("import.rate")));
-
 
 	}
 }
